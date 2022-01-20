d1f97915b23fe54c9a630260aac27d7d03edc1d1
Mario L <m9lopez@ucsd.edu>
2022-01-20 10:06:50 -0800

    some styleing in addNote

--- src/components/Addnote.tsx
+++ src/components/Addnote.tsx
@@ -3,28 +3,53 @@ import { useState } from 'react';
 
 function Addnote() {
   const [spin, setSpin] = useState(true);
+  const [note, setNotes] = useState({});
 
-  const onSpin = () => {
+  const handleSpin = () => {
     setSpin(!spin);
   };
 
+
+  function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+    console.log('hello')
+  }
+
+  type ButtonProps = {
+
+  }
+
   return (
-    <div>
+      <>
+    <div className='flex justify-end'>
       <BsPlusLg
         className={
           spin === true
-            ? 'text-3xl transition'
-            : 'text-3xl rotate-45 transition'
+            ? 'text-3xl transition' 
+            : 'text-3xl rotate-45 justify-end transition'
         }
-        onClick={onSpin}
-      />
-
+        onClick={handleSpin}
+      /></div>
+<div className='flex justify-center'>
         {!spin && 
         <>
         <h1 className='transition'>hhdhdhd</h1>
-        </>}
+        <form onSubmit={e => {handleSubmit(e)}}>
+            <input type="text" placeholder='Note Title' id="title" />
 
-    </div>
+            <input type="text" placeholder='Note details' id="note details"/>
+
+
+
+<button type='submit' >
+            Send
+          </button>
+        </form>
+
+        
+        </>}</div>
+</>
+    
   );
 }
 
